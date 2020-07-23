4813c29e45eb7fa18905547700382056b67519f3
Marion <marion.velard@gmail.com>
2020-07-23 17:14:34 +0200

    Class FizzBuzz refacto

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,14 +1,31 @@
-def fizz_buzz(num)
-  resultat = ''
-  resultat << 'fizz' if contient_ou_multiple(num, 3)
-  resultat << 'buzz' if contient_ou_multiple(num, 5)
+class FizzBuzz
+  def initialize(num)
+    @num = num
+  end
 
-  resultat == '' ? num : resultat
+  def traduis
+    resultat = ''
+    resultat << 'fizz' if contient_ou_multiple(3)
+    resultat << 'buzz' if contient_ou_multiple(5)
+
+    resultat == '' ? @num : resultat
+  end
+
+  def contient_ou_multiple(multiple)
+    multiple?(multiple) || contient?(multiple)
+  end
+
+  def multiple?(multiple)
+    (@num % multiple == 0)
+  end
+
+  def contient?(multiple)
+    @num.to_s.include?(multiple.to_s)
+  end
 end
 
-def contient_ou_multiple(num, multiple)
-  (num % multiple == 0) ||
-    num.to_s.include?(multiple.to_s)
+def fizz_buzz(num)
+  FizzBuzz.new(num).traduis
 end
 
 def affiche_fizz_buzz
