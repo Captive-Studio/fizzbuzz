2d8e10ba152d326cbeb7d2fdf6d618b34108d57d
Stéphane Hanser <shanser@captive.fr>
2020-07-23 16:32:23 +0200

    affiche fizz_buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,12 @@
 def fizz_buzz(num)
-  return "buzz" if num == 5
+  return 'buzz' if num == 5
+
+  return 'fizz' if num == 3
+
+  num
+end
+
+
+def affiche_fizz_buzz
+  100.times { |n| p fizz_buzz(n + 1) }
 end
--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -4,4 +4,5 @@ describe 'FizzBuzz' do
   it { expect(fizz_buzz(1)).to eq(1) }
   it { expect(fizz_buzz(5)).to eq('buzz')}
   it { expect(fizz_buzz(2)).to eq(2)}
+  it { expect(fizz_buzz(3)).to eq('fizz')}
 end
