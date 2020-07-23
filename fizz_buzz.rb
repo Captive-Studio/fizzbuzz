5689add9b257eda25e891c6a887b4a93f50401bd
Marine Sourin <msourin@captive.fr>
2020-07-23 16:38:23 +0200

    Multiple de 3 et de 5. WIP fizzbuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,10 @@
 def fizz_buzz(num)
-  return 'buzz' if num == 5
+  return 'buzz' if (num % 5 == 0)
 
-  return 'fizz' if num == 3
+  return 'fizz' if (num % 3 == 0)
 
   num
 end
-
 
 def affiche_fizz_buzz
   100.times { |n| p fizz_buzz(n + 1) }
--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -2,7 +2,10 @@ require './fizz_buzz.rb'
 
 describe 'FizzBuzz' do
   it { expect(fizz_buzz(1)).to eq(1) }
-  it { expect(fizz_buzz(5)).to eq('buzz')}
-  it { expect(fizz_buzz(2)).to eq(2)}
-  it { expect(fizz_buzz(3)).to eq('fizz')}
+  it { expect(fizz_buzz(5)).to eq('buzz') }
+  it { expect(fizz_buzz(2)).to eq(2) }
+  it { expect(fizz_buzz(3)).to eq('fizz') }
+  it { expect(fizz_buzz(10)).to eq('buzz') }
+  it { expect(fizz_buzz(6)).to eq('fizz') }
+  it { expect(fizz_buzz(15)).to eq('fizzbuzz') }
 end
